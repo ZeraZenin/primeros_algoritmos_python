4d5dcdf9edf6045e12b7c9db519b30dd1fc23002
ZeraZenin <gutzchermain18@gmail.com>
2026-06-11 11:20:57 -0600

    Delete mayor-o-menor-de-edad.py.py

--- mayor-o-menor-de-edad.py.py
+++
@@ -1,8 +0,0 @@
-nombre = input ("¿Cuál es tu nomre?")
-edad = int(input ("¿Cuál es tu edad?"))
-if edad >= 18:
-    print (f"{nombre}, eres mayor de edad, ya hay que empezar a cuidarse las rodillas!")
-elif edad >= 13:
-    print (f"{nombre}, eres un adolescente, quiza rebelde, pero disfruta tu juventud!")
-else: 
-    print (f"{nombre}, eres menor de edad, disfruta tu infancia ")
