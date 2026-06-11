5b4e5277913f4d416add84297529123007f4f2cd
ZeraZenin <gutzchermain18@gmail.com>
2026-06-11 11:24:16 -0600

    primeros_proyectos_python

---
+++ calculadora_edad.py
@@ -0,0 +1,9 @@
+print ("=== Calculadora de edad ===\n")
+nombre = input ("¿Como te llamas?")
+edad= int(input ("¿Cuantos años tienes?"))
+anio_actual = 2026
+print (f"\nHola {nombre}!")
+print (f"Tienes {edad} años.")
+print (f"Naciste aproximadamente en el año {anio_actual - edad}.")
+print (f"El año que viene tendrás {edad + 1} años.")
+print (f"En 10 años tendrás {edad + 10} años.")
---
+++ hola.py
@@ -0,0 +1,4 @@
+print ("Hola, python esta funcionando en VS code")
+nombre =input("¿Cual es tu nombre? ")
+edad =int(input("¿Cual es tu edad? "))
+print (f"Hola {nombre}, tu edad es {edad} años")
